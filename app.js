a1974b4e73c0eaabd107b424e832ac31ccbf90ae
sub_zero <ostapovici.anton@gmail.com>
2021-03-10 16:47:26 +0200

    Set the query and mutation for user , make relation between users and events

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const PORT = 3000;
 const app = express();
@@ -33,12 +35,24 @@ app.use(
             price:Float!
             date:String!  
         }
+
+        type User{
+            _id : ID!
+            email:String!
+            password :String
+            
+        }
         
         input EventInput{
             title :String!
             description :String!
             price:Float!
             date:String! 
+        }
+        input UserInput{
+            email:String!
+            password:String!
+    
         }
 
         type RootQuery{
@@ -47,12 +61,12 @@ app.use(
 
         type RootMutation{
             createEvent(eventInput : EventInput):Event
+            createUser(userInput : UserInput):User
 
         }
 
         schema {
             query:RootQuery
-            
             mutation: RootMutation
         }
     `),
@@ -76,16 +90,62 @@ app.use(
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: new Date(args.eventInput.date),
+          //pass the ObjectId  of  the creator(user)
+          creator: "6048d6956d0d3613f59fedea",
         });
+        let createdEvent;
         //store in DATABAASE
         //save() return a kind of Promise , async
         return event
           .save()
           .then((result) => {
-            console.log(result);
             //to recieve just information we need , without metadataa , will distructuring this objects
-            //In mongo db , "id" it's a special type  to have access to it need to convert/destructuring   
-            return { ...result._doc, _id: event.id };
+            //In mongo db , "id" it's a special type  to have access to it need to convert/destructuring
+            createdEvent = { ...result._doc, password: null, _id: result.id };
+            console.log(result);
+
+            return User.findById("6048d60d774bdf4eee42deb4");
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error("User not found !");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            return createdEvent;
+          })
+          .catch((err) => {
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        // to not store 2 user with same email;
+        //find one input - findOne where email from database = email from input
+        return User.findOne({
+          email: args.userInput.email,
+        })
+          .then((user) => {
+            if (user) {
+              throw new Error("User exists alreaady!");
+            }
+            //we won't store password as plain string
+            // will use a package bcrypt to hashing password and salting = 12 ~ (12 levels )
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            //Async Function
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            //as at events to receive information and distructuring id
+            // to not return the password , password will be null  when i will return from database
+            return { ...result._doc, password: null, _id: result.id };
           })
           .catch((err) => {
             throw err;
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  //to knnow which user create which event(booking) || relations ( user - events(booking));
+  createdEvents: [
+    //store a list//array of id;
+    //ref to event model
+    //describe one single element of this array;
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
