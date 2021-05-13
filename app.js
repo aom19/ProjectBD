5df585f8a3c46fd1f2d67341880f5b271c3743e0
sub_zero <ostapovici.anton@gmail.com>
2021-05-13 23:50:53 +0300

    make

--- app.js
+++ app.js
@@ -48,10 +48,12 @@ app.use(
   })
 );
 
-app.use(express.static('public' ));
+app.use(express.static(path.join(__dirname , "client" , "build" )));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname,  "client" , "build", "index.html"));
 });
+
+
 
 mongoose
   .connect(
